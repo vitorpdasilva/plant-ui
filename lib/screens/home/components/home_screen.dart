862978ee4194514f45d90bddebd16e0b51a01fed
Vitor Boccio <vitorboccio@gmail.com>
2020-08-21 15:20:26 -0400

    package svg

---
+++ lib/screens/home/components/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(icon: SvgPicture.asset("assets/icons/menu.svg"), onPressed: () {}),
+      ),
+    );
+  }
+}
