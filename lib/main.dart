06d3d6e9407b456dbabbd017f2397ed48d35e14e
Vitor Boccio <vitorboccio@gmail.com>
2020-08-23 20:27:22 -0400

    home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plant_ui/constants.dart';
-import 'package:plant_ui/screens/home/components/home_screen.dart';
+import 'package:plant_ui/screens/home/home_screen.dart';
 
 void main() {
   runApp(MyApp());
